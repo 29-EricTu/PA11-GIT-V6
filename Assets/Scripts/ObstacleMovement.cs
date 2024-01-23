e12d796713d7b1013fd1a0f88bc7a9b984964e43
29-EricTu <erictujiaxin48@gmail.com>
2024-01-23 22:49:55 +0800

    update

---
+++ Assets/Scripts/ObstacleMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMovement : MonoBehaviour
+{
+    public Player player;
+    private float xSpeed = -4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(xSpeed*Time.deltaTime, 0, 0f));
+        if (transform.position.y < -5)
+        {
+            Destroy(this.gameObject);
+        }
+        if (transform.position.x <= -9)
+        {
+            player.Score++;
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public Text ScoreText;
+    public int Score;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreText.text = ("SCORE : ") + Score.ToString();
+        if (transform.position.y >= 3)
+        {
+            transform.position = new Vector3(transform.position.x, 3, 0);
+        }
+
+        if (transform.position.y <= -3)
+        {
+            transform.position = new Vector3(transform.position.x, -3, 0);
+        }
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameLose");
+        }
+    }
+}
